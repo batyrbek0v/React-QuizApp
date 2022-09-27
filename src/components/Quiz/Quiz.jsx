2b7bfc985d0b4d151fde79c31fb933fa68d710ca
unknown <erlankambarov94@gmail.com>
2022-09-27 23:18:48 +0600

    responsived the blocks

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,48 +1,8 @@
 import React from 'react'
 import cls from './Quiz.module.scss'
-import question from '../../assets/img/question.png'
+import { questions } from './Utils/Questions'
 
 const Quiz = () => {
-
-  const questions = [
-    {
-      questionText: 'Столица США',
-      answerOptions: [
-        { answerText: 'Бостон', isCorrect: false },
-        { answerText: 'Нью-Йорк', isCorrect: false },
-        { answerText: 'Вашингтон', isCorrect: true },
-        { answerText: 'Флорида', isCorrect: false },
-      ]
-    },
-    {
-      questionText: 'Как меня зовут',
-      answerOptions: [
-        { answerText: 'Эрлан', isCorrect: true },
-        { answerText: 'Дони', isCorrect: false },
-        { answerText: 'Доку', isCorrect: false },
-        { answerText: 'Баха', isCorrect: false },
-      ]
-    },
-    {
-      questionText: 'Столица Кыргызстана',
-      answerOptions: [
-        { answerText: 'Ош', isCorrect: false },
-        { answerText: 'Бишкек', isCorrect: true },
-        { answerText: 'Талас', isCorrect: false },
-        { answerText: 'Ысык-Кол', isCorrect: false },
-      ]
-    },
-    {
-      questionText: 'Формула Воды',
-      answerOptions: [
-        { answerText: 'H2O', isCorrect: true },
-        { answerText: 'NaCo2', isCorrect: false },
-        { answerText: 'H2SO4', isCorrect: false },
-        { answerText: 'CaCH', isCorrect: false },
-      ]
-    },
-  ]
-
 
   const [currentQuestion, setCurrentQuestion] = React.useState(0)
   const [score, setScore] = React.useState(0)
@@ -72,10 +32,10 @@ const Quiz = () => {
             ? <div className={cls.section_score}>
               <h2>Правильных ответов {score} из {questions.length}</h2>
               <div className={cls.right_answers}>
-                <ul>
+                <ul className={cls.mainList}>
                   {
                     questions.map(({ questionText, answerOptions }, i) => (
-                      <ul key={i}>
+                      <ul className={cls.innerList} key={i}>
                         <h2 className={cls.questionText}>
                           {questionText}?
                         </h2>
---
+++ src/components/Quiz/Utils/Questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+  {
+    questionText: 'Столица США',
+    answerOptions: [
+      { answerText: 'Бостон', isCorrect: false },
+      { answerText: 'Нью-Йорк', isCorrect: false },
+      { answerText: 'Вашингтон', isCorrect: true },
+      { answerText: 'Флорида', isCorrect: false },
+    ]
+  },
+  {
+    questionText: 'Как меня зовут',
+    answerOptions: [
+      { answerText: 'Эрлан', isCorrect: true },
+      { answerText: 'Дони', isCorrect: false },
+      { answerText: 'Доку', isCorrect: false },
+      { answerText: 'Баха', isCorrect: false },
+    ]
+  },
+  {
+    questionText: 'Столица Кыргызстана',
+    answerOptions: [
+      { answerText: 'Ош', isCorrect: false },
+      { answerText: 'Бишкек', isCorrect: true },
+      { answerText: 'Талас', isCorrect: false },
+      { answerText: 'Ысык-Кол', isCorrect: false },
+    ]
+  },
+  {
+    questionText: 'Формула Воды',
+    answerOptions: [
+      { answerText: 'H2O', isCorrect: true },
+      { answerText: 'NaCo2', isCorrect: false },
+      { answerText: 'H2SO4', isCorrect: false },
+      { answerText: 'CaCH', isCorrect: false },
+    ]
+  },
+]
